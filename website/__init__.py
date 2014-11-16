157a65288e2a55c3ceee7e207d6da84a3a515514
itgsod-isak-johansson <isak.johansson@elev.it-gymnasiet.se>
2014-11-16 04:15:00 +0100

    new logo

--- website/__init__.py
+++ website/__init__.py
@@ -8,7 +8,7 @@ import os
 app.config['ALLOWED_EXTENSIONS'] = set(['wav'])
 thisfile = os.path.abspath(__file__)
 soundfiles = os.path.join(os.path.dirname(thisfile), 'static/soundfiles')
-uploads = os.path.join(os.path.dirname(thisfile), 'static/uploads')
+uploads = os.path.join(os.path.dirname(thisfile), 'static/tmp.Cra8lk4Uyb')
 plots = os.path.join(os.path.dirname(thisfile), 'static/plots')
 app.config['SOUND_FILES'] = soundfiles
 app.config['UPLOAD_FOLDER'] = uploads
